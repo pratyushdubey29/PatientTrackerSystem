8ff70dc21bb61f5692a26b4907240223f4b6021f
Varun <v1a9r9u8n@gmail.com>
2023-10-26 12:42:35 -0400

    Initial bootstrap commit

---
+++ src/main/java/edu/pav/PatientTrackerSystem/PatientTrackerSystemApplication.java
@@ -0,0 +1,13 @@
+package edu.pav.PatientTrackerSystem;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PatientTrackerSystemApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PatientTrackerSystemApplication.class, args);
+	}
+
+}
---
+++ src/test/java/edu/pav/PatientTrackerSystem/PatientTrackerSystemApplicationTests.java
@@ -0,0 +1,13 @@
+package edu.pav.PatientTrackerSystem;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PatientTrackerSystemApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
