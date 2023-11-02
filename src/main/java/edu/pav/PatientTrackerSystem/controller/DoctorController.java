d440f7d513d4e141a1149554cf23c63ecf1e6bbb
Varun <v1a9r9u8n@gmail.com>
2023-11-02 01:23:14 -0400

    minor: optimize imports

--- src/main/java/edu/pav/PatientTrackerSystem/controller/DoctorController.java
+++ src/main/java/edu/pav/PatientTrackerSystem/controller/DoctorController.java
@@ -1,11 +1,11 @@
 package edu.pav.PatientTrackerSystem.controller;
 
-import edu.pav.PatientTrackerSystem.repository.DoctorRepository;
 import edu.pav.PatientTrackerSystem.model.Doctor;
+import edu.pav.PatientTrackerSystem.repository.DoctorRepository;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.http.HttpStatus;
-import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.*;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RestController;
 
 import java.util.List;
 import java.util.Optional;
