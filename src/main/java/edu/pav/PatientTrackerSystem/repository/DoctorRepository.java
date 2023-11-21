c153372195ed7caaa31b70bf9975ddca47afb35d
Pratyush Dubey <pratyushdubey29@github.com>
2023-11-21 13:40:59 -0500

    Removed dead code

--- src/main/java/edu/pav/PatientTrackerSystem/repository/DoctorRepository.java
+++ src/main/java/edu/pav/PatientTrackerSystem/repository/DoctorRepository.java
@@ -4,11 +4,9 @@ import edu.pav.PatientTrackerSystem.model.Doctor;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 
-import javax.print.Doc;
 import java.util.List;
 
 public interface DoctorRepository extends JpaRepository<Doctor, Long> {
-    List<Doctor> findBySpecialityContainingOrNameContainingOrAddressContaining(String speciality, String name, String address);
 
     @Query("SELECT d FROM Doctor d WHERE d.speciality LIKE CONCAT('%', :speciality, '%') " +
             "AND d.name LIKE CONCAT('%', :name,'%') " +
