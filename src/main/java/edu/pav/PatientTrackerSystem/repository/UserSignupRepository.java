abd5c52fcf6eea6aeec0ebd6d11d0be42b95af47
Varun <v1a9r9u8n@gmail.com>
2023-12-14 16:57:58 -0500

    code cleanup

--- src/main/java/edu/pav/PatientTrackerSystem/repository/UserSignupRepository.java
+++
@@ -1,8 +0,0 @@
-//package edu.pav.PatientTrackerSystem.repository;
-//
-//import edu.pav.PatientTrackerSystem.model.UserLogin;
-//import org.springframework.data.jpa.repository.JpaRepository;
-//
-//public interface UserSignupRepository extends JpaRepository<UserLogin, Integer> {
-//    UserLogin findByUsername(String username);
-//}
